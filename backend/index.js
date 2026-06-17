3a117dda3404092767280b94c1d22fa6d3f5156c
Staylearner <rohanghosh078@gmail.com>
2026-06-17 14:23:29 +0530

    fixes for deployment

--- backend/index.js
+++ backend/index.js
@@ -46,11 +46,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        connectSrc: ["'self'", "https://career-canvas.onrender.com",
-          "https://www.careercanvas.online",
-          "https://careercanvas.online",
-          "https://career-canvas-development.onrender.com",
-          "https://career-canvas-old-ui.onrender.com"],
+        connectSrc: ["'self'", "https://career-canvas.onrender.com","https://www.careercanvas.online","https://careercanvas.online","https://career-canvas-development.onrender.com","https://career-canvas-old-ui.onrender.com"],
         imgSrc: ["'self'", "data:", "https:"],
         scriptSrc: ["'self'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https:"],
